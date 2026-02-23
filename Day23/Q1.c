2bc758a2302c2cf488dadc3e62a9f4425eb5dcb5
Anoushka_Saini <anoushkasaini2007@gmail.com>
2026-02-23 11:24:45 +0530

    Create Q1.c

---
+++ Day23/Q1.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to insert at end
+struct Node* insert(struct Node* head, int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (head == NULL)
+        return newNode;
+
+    struct Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    return head;
+}
+
+// Function to merge two sorted lists
+struct Node* merge(struct Node* l1, struct Node* l2) {
+
+    struct Node dummy;
+    struct Node* tail = &dummy;
+    dummy.next = NULL;
+
+    while (l1 != NULL && l2 != NULL) {
+        if (l1->data <= l2->data) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+
+    if (l1 != NULL)
+        tail->next = l1;
+    else
+        tail->next = l2;
+
+    return dummy.next;
+}
+
+int main() {
+    int n, m, value;
+
+    struct Node* head1 = NULL;
+    struct Node* head2 = NULL;
+
+    // First list
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        head1 = insert(head1, value);
+    }
+
+    // Second list
+    scanf("%d", &m);
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &value);
+        head2 = insert(head2, value);
+    }
+
+    // Merge
+    struct Node* merged = merge(head1, head2);
+
+    // Print merged list
+    while (merged != NULL) {
+        printf("%d ", merged->data);
+        merged = merged->next;
+    }
+
+    return 0;
+}
