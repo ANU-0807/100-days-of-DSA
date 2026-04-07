a6310cf19514e376e928231db0c54b3b1e6d6de1
Anoushka_Saini <anoushkasaini2007@gmail.com>
2026-04-07 10:28:48 +0530

    Create Q1.c

---
+++ Day66/Q1.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX 100
+
+int adj[MAX][MAX];
+bool visited[MAX];
+bool recStack[MAX];
+int n;
+
+// DFS function
+bool dfs(int node) {
+    visited[node] = true;
+    recStack[node] = true;
+
+    for (int i = 0; i < n; i++) {
+        if (adj[node][i]) {
+            if (!visited[i]) {
+                if (dfs(i))
+                    return true;
+            }
+            else if (recStack[i]) {
+                return true; // cycle detected
+            }
+        }
+    }
+
+    recStack[node] = false; // backtrack
+    return false;
+}
+
+int main() {
+    int e, u, v;
+
+    printf("Enter number of vertices and edges: ");
+    scanf("%d %d", &n, &e);
+
+    // initialize
+    for (int i = 0; i < n; i++) {
+        visited[i] = false;
+        recStack[i] = false;
+        for (int j = 0; j < n; j++) {
+            adj[i][j] = 0;
+        }
+    }
+
+    printf("Enter directed edges (u v):\n");
+    for (int i = 0; i < e; i++) {
+        scanf("%d %d", &u, &v);
+        adj[u][v] = 1;
+    }
+
+    // check all components
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            if (dfs(i)) {
+                printf("YES\n");
+                return 0;
+            }
+        }
+    }
+
+    printf("NO\n");
+    return 0;
+}
